e68041a5db757857f6aa600415528eb7d596fa05
Stephanie Tjiong <stephanielaurentjiong@gmail.com>
2024-07-16 17:17:59 -0700

    used custom hooks to set useEffect hooks and use fragments instead of div

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,35 @@ import * as React from 'react';
 import TodoList from './TodoList.jsx';
 import AddTodoForm from './AddTodoForm.jsx';
 
+const useSemiPersistentState = () => {
+  const [ todoList, setTodoList ] = React.useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+  );
+  
+  React.useEffect(()=> {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [ todoList, setTodoList ]
+}
+
 
 function App () {
   
-  const [ todoList, setTodoList ] = React.useState([]);
+  const [ todoList, setTodoList ] = useSemiPersistentState('');
   
+
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
   }
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <TodoList todoList={todoList} />
       <AddTodoForm onAddTodo={addTodo}/>
       
-    </div>
+    </>
   );
 }
 
